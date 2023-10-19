34507850f30e3a8a92b43e8cbfd7b3330d45b0bf
Joy Wang <yihan.wang3@ucalgary.ca>
2023-10-19 11:25:35 -0600

    - updated test

--- TestInspector.java
+++ TestInspector.java
@@ -21,11 +21,21 @@ public class TestInspector {
     @Test
     public void TestInspectArray()
     {
+        ArrayInfo info1 = new ArrayInfo();
+        ArrayInfo info2 = new ArrayInfo();
         int[] testList = {1,2,3,4,5};
         String[] testList2 = {"Hi", "This", "Is", "Karina"};
         Inspector isp = new Inspector();
-        isp.InspectArray(testList);
-        isp.InspectArray(testList2);
+        isp.InspectArray(testList, info1);
+        isp.InspectArray(testList2, info2);
+    }
+
+    @Test
+    public void TestInspect2DArray()
+    {
+        int[][] testList = {{1,2},{3,4}};
+        Inspector isp = new Inspector();
+        isp.InspectMultipleDimensionalArray(testList);
     }
 
 
